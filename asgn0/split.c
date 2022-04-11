85dfb5c0344d64842c7729e19baa8ba24dc8d517
Tanishq Patil <tmpatil@ucsc.edu>
2022-04-11 22:04:42 +0000

    Replace split.c

--- asgn0/split.c
+++ asgn0/split.c
@@ -1,91 +1,3 @@
-/*
-#include <err.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[]) {
-
-    
-    if (argc < 3) {
-
-        errx(22, "Not enough arguments\nusage: ./split: <split_char> [<file1> "
-                 "<file2> ...]");
-    }
-
-    if (strlen(argv[1]) > 1) {
-
-        errx(22,
-            "Cannot handle multi-character splits: %s\nusage: ./split: "
-            "<split_char> [<file1> "
-            "<file2> ...]",
-            argv[1]);
-    }
-
-    int i = 1;
-
-    int red;
-
-    char *delim = argv[1];
-
-    char buffer[100];
-
-    i++;
-    while (i < argc) {
-
-        int fd = 2;
-
-        if (strcmp(argv[i], "-") == 0) {
-            fd = 0;
-        }
-
-        else {
-            fd = open(argv[i], O_RDONLY);
-        }
-
-        if (fd == -1) {
-
-            errx(2, "split: %s: No such file or directory", argv[i]);
-        }
-
-        while ((red = read(fd, buffer, 99)) > 0) {
-
-            for (int ctr = 0; ctr < red; ctr++) {
-                if (buffer[ctr] == delim[0]) {
-                    buffer[ctr] = '\n';
-                }
-            }
-
-            buffer[red] = '\0';
-
-            if ((write(STDOUT_FILENO, buffer, red)) == -1) {
-                perror("write");
-                exit(1);
-            }
-
-            if (red == -1) {
-                perror("read");
-                exit(1);
-            }
-        }
-
-        if (close(fd) == -1) {
-            perror("close");
-            exit(1);
-        }
-
-        i++;
-    }
-
-    return 0;
-}
-*/
-
-//--- hope so the better version ----
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -121,7 +33,7 @@ int main(int argc, char *argv[]) {
 
     int bytes_exp;
 
-    char* buffer;
+    char *buffer;
 
     struct stat buf;
 
@@ -143,24 +55,64 @@ int main(int argc, char *argv[]) {
             errx(2, "split: %s: No such file or directory", argv[i]);
         }
 
-// When input is stdin
+        // When input is stdin
 
+        if (fd == 0) {
 
-        if(fd == 0){
+            char buffer_2[100];
 
-        char buffer_2[100]; 
+            while ((red = read(fd, buffer_2, 99)) > 0) {
 
-        while ((red = read(fd, buffer_2, 99)) > 0) {
+                for (int ctr = 0; ctr < red; ctr++) {
+                    if (buffer_2[ctr] == delim[0]) {
+                        buffer_2[ctr] = '\n';
+                    }
+                }
 
-            for (int ctr = 0; ctr < red; ctr++) {
-                if (buffer_2[ctr] == delim[0]) {
-                    buffer_2[ctr] = '\n';
+                buffer_2[red] = '\0';
+
+                if ((write(STDOUT_FILENO, buffer_2, red)) == -1) {
+                    perror("write");
+                    exit(1);
+                }
+
+                if (red == -1) {
+                    perror("read");
+                    exit(1);
                 }
             }
 
-            buffer_2[red] = '\0';
+            if (close(fd) == -1) {
+                perror("close");
+                exit(1);
+            }
+        }
 
-            if ((write(STDOUT_FILENO, buffer_2, red)) == -1) {
+        // other cases
+
+        else {
+
+            fstat(fd, &buf);
+
+            bytes_exp = buf.st_size;
+
+            buffer = (char *) calloc(bytes_exp + 1, sizeof(char));
+
+            if (buffer == NULL) {
+                errx(2, "Memory not allocated.\n");
+            }
+
+            red = read(fd, buffer, bytes_exp);
+
+            for (int ctr = 0; ctr < red; ctr++) {
+                if (buffer[ctr] == delim[0]) {
+                    buffer[ctr] = '\n';
+                }
+            }
+
+            buffer[red] = '\0';
+
+            if ((write(STDOUT_FILENO, buffer, red)) == -1) {
                 perror("write");
                 exit(1);
             }
@@ -169,55 +121,13 @@ int main(int argc, char *argv[]) {
                 perror("read");
                 exit(1);
             }
-        }
 
-        if (close(fd) == -1) {
-            perror("close");
-            exit(1);
-        }
-        }
-
-        // other cases
-
-        else{
-
-        fstat(fd, &buf);
-
-        bytes_exp = buf.st_size;
-
-        buffer = (char *)calloc(bytes_exp+1, sizeof(char));
-
-        if(buffer == NULL){
-            errx(2, "Memory not allocated.\n");
-        }
-
-        red = read(fd, buffer, bytes_exp);
- 
-        for (int ctr = 0; ctr < red; ctr++) {
-            if (buffer[ctr] == delim[0]) {
-                buffer[ctr] = '\n';
+            if (close(fd) == -1) {
+                perror("close");
+                exit(1);
             }
-        }
 
-        buffer[red] = '\0';
-
-        if ((write(STDOUT_FILENO, buffer, red)) == -1) {
-            perror("write");
-            exit(1);
-        }
-
-        if (red == -1) {
-            perror("read");
-            exit(1);
-        }
-
-
-        if (close(fd) == -1) {
-            perror("close");
-            exit(1);
-        }
-
-        free(buffer);
+            free(buffer);
         }
 
         i++;
