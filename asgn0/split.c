e6ad1f07633893ca334e8fb2e4153e9d28a0e042
Tanishq Patil <tmpatil@ucsc.edu>
2022-04-11 21:28:26 +0000

    Update split.c

--- asgn0/split.c
+++ asgn0/split.c
@@ -1,3 +1,4 @@
+/*
 #include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,7 +8,7 @@
 
 int main(int argc, char *argv[]) {
 
-    // not enough arguments
+    
     if (argc < 3) {
 
         errx(22, "Not enough arguments\nusage: ./split: <split_char> [<file1> "
@@ -80,3 +81,109 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+*/
+
+//--- hope so the better version ----
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <err.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+    // not enough arguments
+    if (argc < 3) {
+
+        errx(22, "Not enough arguments\nusage: ./split: <split_char> [<file1> "
+                 "<file2> ...]");
+    }
+
+    if (strlen(argv[1]) > 1) {
+
+        errx(22,
+            "Cannot handle multi-character splits: %s\nusage: ./split: "
+            "<split_char> [<file1> "
+            "<file2> ...]",
+            argv[1]);
+    }
+
+    int i = 1;
+
+    int red;
+
+    char *delim = argv[1];
+
+    int bytes_exp;
+
+    char* buffer;
+
+    struct stat buf;
+
+    i++;
+    while (i < argc) {
+
+        int fd = 2;
+
+        if (strcmp(argv[i], "-") == 0) {
+            fd = 0;
+        }
+
+        else {
+            fd = open(argv[i], O_RDONLY);
+        }
+
+        if (fd == -1) {
+
+            errx(2, "split: %s: No such file or directory", argv[i]);
+        }
+
+        fstat(fd, &buf);
+
+        bytes_exp = buf.st_size;
+
+        buffer = (char *)calloc(bytes_exp+1, sizeof(char));
+
+        if(buffer == NULL){
+            errx(2, "Memory not allocated.\n");
+        }
+
+        red = read(fd, buffer, bytes_exp);
+ 
+        for (int ctr = 0; ctr < red; ctr++) {
+            if (buffer[ctr] == delim[0]) {
+                buffer[ctr] = '\n';
+            }
+        }
+
+        buffer[red] = '\0';
+
+        if ((write(STDOUT_FILENO, buffer, red)) == -1) {
+            perror("write");
+            exit(1);
+        }
+
+        if (red == -1) {
+            perror("read");
+            exit(1);
+        }
+
+
+        if (close(fd) == -1) {
+            perror("close");
+            exit(1);
+        }
+
+        free(buffer);
+
+        i++;
+    }
+
+    return 0;
+}
+
