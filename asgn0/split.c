553d6ab42b747f6c5ac8fb7ad1407afe6846e3b0
Tanishq Patil <tmpatil@ucsc.edu>
2022-04-11 23:19:17 +0000

    Replace split.c

--- asgn0/split.c
+++ asgn0/split.c
@@ -7,76 +7,76 @@
 
 int main(int argc, char *argv[]) {
 
-  // not enough arguments
-  if (argc < 3) {
+    // not enough arguments
+    if (argc < 3) {
 
-    errx(22, "Not enough arguments\nusage: ./split: <split_char> [<file1> "
-             "<file2> ...]");
-  }
-
-  if (strlen(argv[1]) > 1) {
-
-    errx(22,
-         "Cannot handle multi-character splits: %s\nusage: ./split: "
-         "<split_char> [<file1> "
-         "<file2> ...]",
-         argv[1]);
-  }
-
-  int i = 1;
-
-  int red;
-
-  char *delim = argv[1];
-
-  char buffer[2048];
-
-  i++;
-  while (i < argc) {
-
-    int fd = 2;
-
-    if (strcmp(argv[i], "-") == 0) {
-      fd = 0;
+        errx(22, "Not enough arguments\nusage: ./split: <split_char> [<file1> "
+                 "<file2> ...]");
     }
 
-    else {
-      fd = open(argv[i], O_RDONLY);
+    if (strlen(argv[1]) > 1) {
+
+        errx(22,
+            "Cannot handle multi-character splits: %s\nusage: ./split: "
+            "<split_char> [<file1> "
+            "<file2> ...]",
+            argv[1]);
     }
 
-    if (fd == -1) {
+    int i = 1;
 
-      errx(2, "split: %s: No such file or directory", argv[i]);
-    }
+    int red;
 
-    while ((red = read(fd, buffer, 2043)) > 0) {
+    char *delim = argv[1];
 
-      for (int ctr = 0; ctr < red; ctr++) {
-        if (buffer[ctr] == delim[0]) {
-          buffer[ctr] = '\n';
-        }
-      }
-
-      buffer[red] = '\0';
-
-      if ((write(STDOUT_FILENO, buffer, red)) == -1) {
-        perror("write");
-        exit(1);
-      }
-
-      if (red == -1) {
-        perror("read");
-        exit(1);
-      }
-    }
-
-    if (close(fd) == -1) {
-      perror("close");
-      exit(1);
-    }
+    char buffer[2048];
 
     i++;
-  }
+    while (i < argc) {
 
-  return 0;
+        int fd = 2;
+
+        if (strcmp(argv[i], "-") == 0) {
+            fd = 0;
+        }
+
+        else {
+            fd = open(argv[i], O_RDONLY);
+        }
+
+        if (fd == -1) {
+
+            errx(2, "split: %s: No such file or directory", argv[i]);
+        }
+
+        while ((red = read(fd, buffer, 2043)) > 0) {
+
+            for (int ctr = 0; ctr < red; ctr++) {
+                if (buffer[ctr] == delim[0]) {
+                    buffer[ctr] = '\n';
+                }
+            }
+
+            buffer[red] = '\0';
+
+            if ((write(STDOUT_FILENO, buffer, red)) == -1) {
+                perror("write");
+                exit(1);
+            }
+
+            if (red == -1) {
+                perror("read");
+                exit(1);
+            }
+        }
+
+        if (close(fd) == -1) {
+            perror("close");
+            exit(1);
+        }
+
+        i++;
+    }
+
+    return 0;
 }
